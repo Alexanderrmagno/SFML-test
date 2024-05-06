9f511876868ce167a33d6d467d84fa08d37f3926
Alexandrrmagno <22192@portalcedi.edu.mx>
2024-05-06 13:10:57 -0600

    oña<3

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <SFML/Graphics.hpp>
+#include "Grid.hpp"
 using namespace sf;
-#include "Square.hpp"
-#include "square.cpp"
-#include <vector>
-
-using namespace std;
-
-Vector2f speed = {3.f, 3.f};
 
 int main()
 {
-    RenderWindow window(VideoMode(1920, 1080), "SFML works!"); // creamos una ventana
-    Square shape(Vector2f(50.f, 50.f));
-    window.setFramerateLimit(120); // el framerate de la pantalla
-    vector<Square> rectangulos;
-
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    Grid grid(5,5);
     while (window.isOpen())
     {
         Event event;
@@ -22,38 +13,10 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
-            if (event.type == Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == Mouse::Left)
-                {
-                    int w = 50;
-                    int h = 50;
-                    int x = event.mouseButton.x;
-                    int y = event.mouseButton.y;
-                    Square rec(Vector2f(w, h), x, y);
-                    rectangulos.push_back(rec);
-                }
-                // evento cambiar un cuadrado a color rojo
-                if (event.mouseButton.button == Mouse::Right)
-                {
-                    int x = event.mouseButton.x;
-                    int y = event.mouseButton.y;
-                    for (auto &r : rectangulos)
-                    {
-                        // r.click(x,y);
-                                        }
-                }
-            }
         }
 
         window.clear();
-        shape.update();
-        shape.drawTo(window);
-        for (auto &r : rectangulos)
-        {
-            r.update();
-            r.drawTo(window);
-        }
+        grid.drawTo(window);
         window.display();
     }
 
