7a846904836ad072fbcbabd63573eb148c59cbcf
Alexandrrmagno <22192@portalcedi.edu.mx>
2024-05-08 14:05:36 -0600

    Vida

--- Grid.cpp
+++ Grid.cpp
@@ -13,9 +13,8 @@ Grid::Grid(int rows, int cols)
         }
     }
 
-this-> siguente = vector<vector<int>>(rows, vector<int>(cols, 0));
+    this->siguiente = vector<vector<int>>(rows, vector<int>(cols, 0));
 }
-
 Grid::Grid(int n, int w, int h)
 {
     this->rows = n;
@@ -27,9 +26,10 @@ Grid::Grid(int n, int w, int h)
         tablero.push_back({});
         for (int j = 0; j < this->cols; j++)
         {
-            tablero[i].push_back(rand() % 2);
+            tablero[i].push_back(0);
         }
     }
+    this->siguiente = vector<vector<int>>(rows, vector<int>(cols, 0));
 }
 
 void Grid::drawTo(RenderWindow &window)
@@ -47,7 +47,7 @@ void Grid::drawTo(RenderWindow &window)
             rect.setOutlineColor(Color::Black);
             if (tablero[j][i] == 1)
             {
-                rect.setFillColor(Color::Black);
+                rect.setFillColor(Color::Green);
             }
             window.draw(rect);
         }
@@ -62,16 +62,79 @@ void Grid::toggle(int x, int y)
     int indexX = x / sizeX;
     int indexY = y / sizeY;
 
-    // if (tablero[indexX][indexY] == 0)
-    // {
-    //     tablero[indexX][indexY] = 1;
-    // }
-    // else
-    // {
-    //     tablero[indexX][indexY] = 0;
-    // }
-
-    // tablero[indexX][indexY] = tablero[indexX][indexY] == 0 ? 1 : 0;
-
     tablero[indexX][indexY] = (tablero[indexX][indexY] + 1) % 2;
+}
+
+void Grid::update()
+{
+    /*
+        para cada celda:
+        calcular cuantos vecinos vivos tiene
+        ver si está viva o muerta en el siguiente a partir de los vecinos
+    */
+
+    for (int i = 0; i < this->rows; i++)
+    {
+        for (int j = 0; j < this->cols; j++)
+        {
+            int vecinos = this->calcularVecinos(i, j);
+            if (this->tablero[i][j] == 1)
+            {
+                if (vecinos < 2 || vecinos > 3)
+                {
+                    this->siguiente[i][j] = 0;
+                }
+                else
+                {
+                    this->siguiente[i][j] = 1;
+                }
+            }
+            else
+            {
+                if (vecinos == 3)
+                {
+                    this->siguiente[i][j] = 1;
+                }
+                else
+                {
+                    this->siguiente[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    this->tablero = this->siguiente;
+}
+
+int Grid::calcularVecinos(int i, int j)
+{
+    int vecinos = 0;
+    // arriba a la izquierda
+    if (i > 0 && j > 0 && this->tablero[i - 1][j - 1] == 1)
+        vecinos++;
+    // arriba
+    if (j > 0 && this->tablero[i][j - 1] == 1)
+        vecinos++;
+    // arriba a la derecha
+    if (j > 0 && i < this->rows - 1 && this->tablero[i + 1][j - 1] == 1)
+        vecinos++;
+
+    // izquierda
+    if (i > 0 && this->tablero[i - 1][j] == 1)
+        vecinos++;
+    // derecha
+    if (i < this->rows - 1 && this->tablero[i + 1][j] == 1)
+        vecinos++;
+
+    // abajo a la izquierda
+    if (i > 0 && j < this->cols - 1 && this->tablero[i - 1][j + 1] == 1)
+        vecinos++;
+    // // abajo
+    if (j < this->cols - 1 && this->tablero[i][j + 1] == 1)
+        vecinos++;
+    // abajo a la derecha
+    if (i < this->rows - 1 && j < this->cols - 1 && this->tablero[i + 1][j + 1] == 1)
+        vecinos++;
+
+    return vecinos;
 }
